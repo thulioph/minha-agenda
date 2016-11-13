22d96005a65407fb7e356f970275bc7e4a5f9a9a
thulioph <thulioph@gmail.com>
2016-11-13 18:36:30 -0300

    Adiciona arquivos estáticos e servidor em node.

---
+++ index.js
@@ -0,0 +1,35 @@
+var express, http, path, app, server;
+
+express = require('express');
+http = require('http');
+path = require('path');
+
+app = express();
+app.set('port', process.env.PORT || 8899);
+
+app.use('/css', express.static(path.join(__dirname, '/app/css/')));
+app.use('/js', express.static(path.join(__dirname, '/app/js/')));
+app.use('/libs/angular', express.static(path.join(__dirname, '/app/vendor/angular/')));
+app.use('/libs/milligram', express.static(path.join(__dirname, '/app/vendor/milligram/dist/')));
+
+server = http.createServer(app);
+
+
+// definindo as rotas
+// ==========
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/app/views/index.html');
+});
+
+app.get('/contatos', function(req, res) {
+  res.sendFile(__dirname + '/app/views/contatos.html');
+});
+
+
+// Startando o server
+// ==========
+
+server.listen(app.get('port'), function() {
+  console.log('Servidor rodando na porta: ', app.get('port'));
+});
