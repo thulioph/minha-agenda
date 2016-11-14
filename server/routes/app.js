2049ff2ebee1d3e15aeac5bd25a15e7e1e422bf5
thulioph <thulioph@gmail.com>
2016-11-13 23:24:50 -0300

    Ajusta arquivo de rotas.

--- server/routes/app.js
+++ server/routes/app.js
@@ -1,116 +1,38 @@
-var http, mongoose;
+var http, db, Contact;
 
-http = require('http'); // módulo para servidor http
-mongoose = require('mongoose'); // módulo para conectar com o mongodb
+http = require('http');
+db = require('config/db');
+Contact = require('controllers/contacts');
 
-// conectando com o mongodb
+
+// Criando o servidor http
 // ==========
-mongoose.connect('mongodb://localhost/cbtu');
-
-var db;
-
-db = mongoose.connection;
-
-db.on('error', function(err) {
-  console.log('Erro de conexão:', err);
-});
-
-db.on('open', function() {
-  console.log('Conexão aberta!');
-});
-
-db.on('connected', function() {
-  console.log('Conectado!');
-});
-
-db.on('disconnected', function() {
-  console.log('Desconectado!');
-});
-
-
-// criando um schema para o banco de dados
-// ==========
-var Schema, ContactSchema, Contact;
-
-Schema = mongoose.Schema;
-
-ContactSchema = new Schema ({
-  name: {
-    type: String,
-    default: '',
-    required: true
-  },
-  phone: {
-    type: String,
-    default: '',
-    required: true
-  },
-  email: {
-    type: String,
-    default: '',
-    required: true
-  },
-  note: {
-    type: String,
-    default: ''
-  },
-  created_at: {
-    type: Date,
-    default: Date.now
-  }
-});
-
-Contact = mongoose.model('Contact', ContactSchema);
-
-
-// inserindo os dados no banco
-// ==========
-
-var data, model, msg;
-
-data = {
-  email: "talita@gmail.com",
-  name: "Talita Albuquerque",
-  note: "Ela é linda <3",
-  phone: "81988755593"
-};
-
-// model = new Contact(data);
-
-// model.save(function(err, data) {
-//   if (err) {
-//     console.log('Ocorreu um erro:', err);
-//   } else {
-//     console.log('Novo contato inserido:', data);
-//   }
-// });
-
-
-// criando o servidor http
-// ==========
-
-function checkUrl(route, response) {
-  if (route === '/contacts/create') {
-    model = new Contact(data);
-
-    model.save(function(err, data) {
-      if (err) {
-        console.log('Ocorreu um erro:', err);
-        msg = 'Error: ' + err;
-      } else {
-        console.log('Novo contato inserido:', data);
-        msg = 'Contato inserido: ' + JSON.stringify(data);
-      }
-
-      response.end(msg);
-    });
-  } else {
-    response.end('Rota não encontrada!');
-  }
-}
 
 http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
-  checkUrl(req.url, res);
+  res.writeHead(200, {
+    'Content-Type': 'text/html;charset=utf-8'
+  });
+
+  switch(req.url) {
+    case '/contacts/create':
+      Contact.create(req, res);
+    break;
+
+    case '/contacts/retrieve':
+      Contact.retrieve(req, res);
+    break;
+
+    case '/contacts/edit':
+      Contact.edit(req, res);
+    break;
+
+    case '/contacts/delete':
+      Contact.delete(req, res);
+    break;
+
+    default: res.end('Rota não encontrada.');
+  }
+
 }).listen(3000);
 
+console.log('Server rodando em -> http://localhost:3000/');
