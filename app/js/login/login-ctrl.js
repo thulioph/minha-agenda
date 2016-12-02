bd891d950a52d930cf531f83e2c9bf2c914e0e01
thulioph <thulioph@gmail.com>
2016-12-02 00:19:13 -0300

    Adiciona método que realiza a autenticação do usuário via Google.

--- app/js/login/login-ctrl.js
+++ app/js/login/login-ctrl.js
@@ -7,7 +7,8 @@
     vm = this;
 
     vm.login = {
-      facebook: loginFacebook
+      facebook: loginFacebook,
+      google: loginGoogle
     };
 
     // ====
@@ -18,12 +19,61 @@
 
     function oAuthUser(evt, obj) {
       $scope.$apply(function () {
-          vm.user_info = obj.user_info;
+          vm.fb_user_info = obj.user_info;
       });
     }
 
-    $rootScope.$on('fb_ok', oAuthUser);
+    function loginGoogle() {
+      console.log('Login com o Google');
 
+      gapi.load('client', _initGogleLogin);
+    }
+
+    function _initGogleLogin() {
+      gapi.client.init({
+        'apiKey': 'AIzaSyDTVFJVhqRTx0-EZADEmMz2giGlyRbzRbw',
+        'discoveryDocs': ['https://people.googleapis.com/$discovery/rest?version=v1'],
+        'clientId': '490787298478-l9t0v16t4es8h8hnset7h18nab6l1gju.apps.googleusercontent.com',
+        'scope': 'profile'
+      }).then(function() {
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+      })
+    }
+
+    function updateSigninStatus(isSignedIn) {
+      if (isSignedIn) {
+        // o usuário já está logado
+        makeApiCall();
+      } else {
+        // o usuário não está logado
+        // este método irá abrir a janela de login
+        gapi.auth2.getAuthInstance().signIn();
+      }
+    }
+
+    function makeApiCall() {
+      gapi.client.people.people.get({
+        resourceName: 'people/me'
+      }).then(function(resp) {
+        console.info(resp.result);
+      });
+    }
+
+    function GetProfileInformation() {
+      var profile = googleUser.getBasicProfile();
+
+      // Do not send to your backend! Use an ID token instead.
+      console.log('ID: ' + profile.getId());
+
+      console.log('Name: ' + profile.getName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail());
+    }
+
+    // ====
+
+    $rootScope.$on('fb_ok', oAuthUser);
   }
 
   LoginCtrl.$inject = [
