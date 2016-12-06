7c98bade2f8894a9c2abdffb659cb193e713b0d8
thulioph <thulioph@gmail.com>
2016-12-06 00:39:17 -0300

    Comenta o código responsável pela autenticação jwt.

--- server/app.js
+++ server/app.js
@@ -6,8 +6,8 @@ logger = require('morgan');
 cookieParser = require('cookie-parser');
 bodyParser = require('body-parser');
 cors = require('cors');
-expressJWT = require('express-jwt');
-jwt = require('jsonwebtoken');
+// expressJWT = require('express-jwt');
+// jwt = require('jsonwebtoken');
 
 // ==========
 
@@ -18,7 +18,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(expressJWT({ secret: 'w3c porra' }).unless({ path: ['/contact'] }));
+// app.use(expressJWT({ secret: 'w3c porra' }).unless({ path: ['/contact'] }));
 
 // ==========
 
