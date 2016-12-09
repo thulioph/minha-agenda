7f4ca757f93b368ca5fadbd2bf58577151dea7e0
thulioph <thulioph@gmail.com>
2016-12-09 01:54:01 -0300

    Ajusta a página de sobre.

--- ionic/barber/www/js/barbearia/controller.js
+++ ionic/barber/www/js/barbearia/controller.js
@@ -2,8 +2,44 @@
   'use strict';
 
   function BarbeariaCtrl() {
-    console.log('BarbeariaCtrl');
+    var vm;
+
+    vm = this;
+
+    // ====
+
+    angular.element(document).ready(function() {
+      initMap();
+    })
+
+    // google.maps.event.addDomListener(window, 'load', initMap);
+
+    // ====
+
+    function initMap() {
+      var map, lat_lng, marker;
+
+      lat_lng = new google.maps.LatLng(-8.1901287,-34.9237563);
+
+      map = new google.maps.Map(document.getElementById('map-container'), {
+        center: lat_lng,
+        zoom: 12,
+        mapTypeControl: false,
+        panControl: false,
+        streetViewControl: false,
+        zoomControl: false,
+        scrollwheel: false,
+        draggable: true
+      });
+
+      marker = new google.maps.Marker({
+        position: lat_lng,
+        map: map
+      });
+    }
   }
+
+  BarbeariaCtrl.$inject = [];
 
   angular
   .module('Barber.barbearia')
