cf91946d49935aaf8dae15343de1b42a8c03ba9e
thulioph <thulioph@gmail.com>
2016-12-11 21:42:10 -0300

    Ajusta os ícones da nav e altera a cor de quando estiver ativo.

--- ionic/barber/www/js/home/controller.js
+++ ionic/barber/www/js/home/controller.js
@@ -1,31 +1,35 @@
 (function() {
   'use strict';
 
-  function HomeCtrl(ApiService) {
+  function HomeCtrl(ApiService, Utils) {
     var vm;
 
     vm = this;
 
-    // angular.element(document).ready(function() {
-    //   getAllClients();
-    // });
+    angular.element(document).ready(function() {
+      getAllClients();
+    });
 
     // ====
 
     vm.lastServices = [];
 
-    // function getAllClients() {
-    //   ApiService.getAllClients().then(function(result) {
-    //     vm.allClients = result;
-    //   });
-    // }
+    function getAllClients() {
+      Utils.progressbar.start();
 
+      ApiService.getAllClients().then(function(result) {
+        vm.allClients = result;
+
+        Utils.progressbar.complete();
+      });
+    }
 
     // ====
   }
 
   HomeCtrl.$inject = [
-    'ApiService'
+    'ApiService',
+    'Utils'
   ];
 
   angular
