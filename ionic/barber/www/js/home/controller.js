b72e81c8194cbea455027e835fbad081e800567f
thulioph <thulioph@gmail.com>
2016-12-11 21:14:46 -0300

    Adiciona botão de sair.

--- ionic/barber/www/js/home/controller.js
+++ ionic/barber/www/js/home/controller.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-  function HomeCtrl(ApiService) {
+  function HomeCtrl(ApiService, $ionicActionSheet) {
     var vm;
 
     vm = this;
@@ -13,6 +13,7 @@
     // ====
 
     vm.allClients = [];
+    vm.quickAccess = quickAccess;
 
     function getAllClients() {
       ApiService.getAllClients().then(function(result) {
@@ -20,11 +21,43 @@
       });
     }
 
+    function _cancel() {
+      console.log('Cancelou!');
+    }
+
+    function _buttonClicked(index) {
+      console.log('Clicou: ', index);
+    }
+
+    function quickAccess() {
+      var options;
+
+      options = new Object();
+
+      options = {
+        buttons: [
+          {
+            text: 'Sim'
+          },
+          {
+            text: 'Não'
+          }
+        ],
+        titleText: 'Deseja sair da Barbearia?',
+        cancelText: 'Cancelar',
+        cancel: _cancel,
+        buttonClicked: _buttonClicked
+      };
+
+      $ionicActionSheet.show(options);
+    }
+
     // ====
   }
 
   HomeCtrl.$inject = [
-    'ApiService'
+    'ApiService',
+    '$ionicActionSheet'
   ];
 
   angular
